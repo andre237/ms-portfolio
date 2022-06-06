c265808824e167cf174457154941580afff11e52
Andre Tibola <andre.spironello12@gmail.com>
2022-06-06 09:53:48 -0300

    feat: include Eureka service discovery

---
+++ ms-discovery-server/src/main/java/com/andre/training/MsDiscoveryEntrypoint.java
@@ -0,0 +1,15 @@
+package com.andre.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsDiscoveryEntrypoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDiscoveryEntrypoint.class, args);
+    }
+
+}
