7aff63bc47522ce2cab436215c8d5f4e7133efe8
Andre Tibola <andre.spironello12@gmail.com>
2022-06-06 09:52:51 -0300

    feat: create sub-library for kafka utilities

---
+++ commons-library/kafka-utils/src/main/java/com/andre/training/schema/BaseSchema.java
@@ -0,0 +1,14 @@
+package com.andre.training.schema;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseSchema {
+
+    private String identifier;
+
+}
