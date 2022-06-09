b0a4846c021481e01150523e2a449da76e619ea9
Andre Tibola <andre.spironello12@gmail.com>
2022-06-09 10:46:01 -0300

    feat: add Spring Cloud Config functionalities

---
+++ ms-config-server/src/main/java/com/andre/training/MsConfigEntrypoint.java
@@ -0,0 +1,15 @@
+package com.andre.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsConfigEntrypoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigEntrypoint.class, args);
+    }
+
+}
--- ms-notifications/src/main/java/com/andre/training/MsNotificationEntrypoint.java
+++ ms-notifications/src/main/java/com/andre/training/MsNotificationEntrypoint.java
@@ -1,13 +1,22 @@
 package com.andre.training;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsNotificationEntrypoint {
+public class MsNotificationEntrypoint implements CommandLineRunner {
+
+    @Value("${app.random-prop}")
+    private String validationProp;
 
     public static void main(String[] args) {
         SpringApplication.run(MsNotificationEntrypoint.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println(" ====> " + validationProp + " <==== ");
+    }
 }
